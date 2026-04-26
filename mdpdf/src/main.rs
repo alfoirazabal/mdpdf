2cb48b552dc5cdc52b4907692f2cf44cfca7740a
alfoirazabal <alfonsoirazabal@gmail.com>
2026-04-26 18:56:47 -0300

    Add `main.rs` to test the current implementation.

--- mdpdf/src/main.rs
+++ mdpdf/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+mod markdown;
+mod template;
+mod pdf;
+mod embed;
+
+use anyhow::Result;
+use std::fs;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let input_path = "examples/sample.md";
+    let output_pdf = "output.pdf";
+
+    let md = fs::read_to_string(input_path)?;
+
+    let html_body = markdown::to_html(&md)?;
+    let full_html = template::wrap_html(&html_body);
+
+    let temp_html = "temp.html";
+
+    println!("Converting to Html...");
+
+    fs::write(temp_html, full_html)?;
+
+    println!("Converting Html to PDF...");
+
+    pdf::html_to_pdf(temp_html, output_pdf).await?;
+
+    println!("Attaching MD to PDF...");
+
+    embed::attach_file(output_pdf, input_path)?;
+
+    println!("Done: {}", output_pdf);
+
+    Ok(())
 }
