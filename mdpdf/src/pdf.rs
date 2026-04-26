2404d187822841bf27196c7447ceed74b94aa3c5
alfoirazabal <alfonsoirazabal@gmail.com>
2026-04-26 18:56:05 -0300

    Add `pdf.rs` with `headless_chrome` to create the PDF.

---
+++ mdpdf/src/pdf.rs
@@ -0,0 +1,32 @@
+use anyhow::Result;
+use headless_chrome::{Browser, LaunchOptions};
+use std::path::Path;
+
+pub async fn html_to_pdf(input: &str, output: &str) -> Result<()> {
+
+    let path = std::fs::canonicalize(Path::new(input))?;
+    let url = format!("file://{}", path.to_string_lossy());
+
+    let browser = Browser::new(LaunchOptions::default())?;
+    let tab = browser.new_tab()?;
+
+    tab.navigate_to(&url)?;
+    tab.wait_until_navigated()?;
+
+    let options = headless_chrome::types::PrintToPdfOptions {
+        margin_top: Some(0.0),
+        margin_bottom: Some(0.0),
+        margin_left: Some(0.0),
+        margin_right: Some(0.0),
+        prefer_css_page_size: Some(true),
+        display_header_footer: Some(false),
+        print_background: Some(true),
+        ..Default::default()
+    };
+
+    let pdf = tab.print_to_pdf(Option::Some(options))?;
+
+    std::fs::write(output, pdf)?;
+
+    Ok(())
+}
